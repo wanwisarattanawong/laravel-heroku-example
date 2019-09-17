a714b66f93d00bd07ff500b8e7633f06e4022439
wanwisarattanawong <51689159+wanwisarattanawong@users.noreply.github.com>
2019-09-17 11:17:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('users', function() {
+    return '<h1>Users! 555</h1>;
+}); 
+Route::get('showname', function() {
+    return '<h1>Hello Laraval</h1>';
+});     
+Route::get('welcome/name',Hellocontroller@index');
