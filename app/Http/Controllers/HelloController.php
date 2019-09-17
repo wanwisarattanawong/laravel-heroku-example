670533aae15adca96a75e2614b077612cd9bcdf7
wanwisarattanawong <51689159+wanwisarattanawong@users.noreply.github.com>
2019-09-17 11:45:38 +0700

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-class HomeController extends Controller
+class HelloController extends Controller
 {
     function showHello(){
        return '<h1>Hello Controller</h1>';
