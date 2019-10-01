5f32e1e55df4d3d7f3a7aaab0a167a935f572077
wanwisarattanawong <51689159+wanwisarattanawong@users.noreply.github.com>
2019-10-01 09:47:32 +0700

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,4 +6,7 @@ class HelloController extends Controller
     function showHello($name){
        return '<h1>Hello:'.$name.'</h1>';
     }
+    function resultProduct($product){
+       return '<h1>Hello:'.$product.'</h1>';
+    }
 }
