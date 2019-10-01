eebfecc5d8292dac76bf77cae9470d3bdee73c88
wanwisarattanawong <51689159+wanwisarattanawong@users.noreply.github.com>
2019-10-01 11:06:12 +0700

    Create master.blade.php

---
+++ resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr"
+  <head
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <style media="screen">
+      body{
+        background-color: yellow;
+      }
+    </style>
+  </head>
+  <body>
+        @yield('content')
+  </body>
+  <footer>@yield('footer')</footer>
+</html
