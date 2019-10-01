8e667d3b8eb0133a4954191f5e6258f683a5f2a2
wanwisarattanawong <51689159+wanwisarattanawong@users.noreply.github.com>
2019-10-01 10:33:04 +0700

    Update users.php

--- resources/views/users.php
+++ resources/views/users.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr"
-  <head
+  <head>
     <meta charset="utf-8">
     <title>Show View</title>
-    </head>
-      <body>
+   </head>
+   <body>
      <h1>View Data :</h1>
+     
   </body>
 </html
