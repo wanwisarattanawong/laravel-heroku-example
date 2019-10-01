b0173199fd05e2c15b94a41f1fa6874338b12e8c
wanwisarattanawong <51689159+wanwisarattanawong@users.noreply.github.com>
2019-10-01 11:39:26 +0700

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,7 +8,7 @@ class HelloController extends Controller
 {
     function show(){
       return view('users')
-      ->with('name','wanwisarattanawong')
+      ->with('name','ploy')
       ->with('title','Laravel tutorial');
   }
 }
